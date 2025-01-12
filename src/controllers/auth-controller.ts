b99764ab2fb1a4c037551e4c50a2d61d8a26d918
Muh-Uzair <muhammaduzair1062001@gmail.com>
2025-01-12 21:47:52 +0500

    user login success

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -2,6 +2,15 @@ import { NextFunction, Request, Response } from "express";
 import { UserInterface, UserModel } from "../models/users-model";
 import { globalAsyncCatch } from "../utils/global-async-catch";
 import jwt from "jsonwebtoken";
+import { AppError } from "../utils/app-error";
+import bcrypt from "bcryptjs";
+
+//FUNCTION
+const signToken = (id: string): string | null => {
+  return id && process.env.JWT_SECRET
+    ? jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" })
+    : null;
+};
 
 // FUNCTION
 export const signup = async (
@@ -17,21 +26,71 @@ export const signup = async (
     if (!process.env.JWT_SECRET) {
       throw new Error("JWT_SECRET is not defined");
     }
-    const token: string = jwt.sign(
-      { id: newUser._id },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "1d",
-      }
-    );
+    const token: string | null = signToken(newUser.id);
+
+    if (!token) {
+      return next(new AppError("Unable to generate jwt token", 500));
+    }
     // 3 : send a response with JWT token
     res.status(200).json({
       status: "success",
       jwtToken: token,
       data: {
-        newUser,
+        //www.youtube.com/watch?list=LL&v=9XaS93WMRQQ
+        https: newUser,
       },
     });
+  } catch (error: unknown) {
+    globalAsyncCatch(error, next);
+  }
+};
+
+//  FUNCTION
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    // 1 : check is req.body contain user name or passwords
+    if (!req.body.email || !req.body.password) {
+      next(new AppError("Please provide email and passwords", 400));
+    } else {
+      console.log(req.body.email, req.body.password);
+
+      // 2 : check is user exists
+      const user: UserInterface | null = await UserModel.findOne({
+        email: req.body.email,
+      }).select("+password");
+
+      console.log(user);
+
+      // 3 : compare the received password with password in db
+      const passwordCorrect = user?.password
+        ? await bcrypt.compare(req.body.password, user.password)
+        : false;
+
+      console.log("_____________________________________________________");
+      console.log(passwordCorrect);
+
+      // 4 : send response accordingly
+      if (!user || !passwordCorrect) {
+        next(new AppError("Incorrect email or password", 400));
+      } else {
+        const token: string | null = signToken(user._id as string);
+
+        if (!token) {
+          return next(new AppError("Unable to generate jwt token", 500));
+        }
+        res.status(200).json({
+          status: "success",
+          token,
+          data: {
+            user,
+          },
+        });
+      }
+    }
   } catch (error: unknown) {
     globalAsyncCatch(error, next);
   }
--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
 import { getAllUsers } from "../controllers/users-controller";
-import { signup } from "../controllers/auth-controller";
+import { login, signup } from "../controllers/auth-controller";
 
 const router: Router = express.Router();
 
-router.route("/signup").post(signup);
 router.route("/").get(getAllUsers);
+router.route("/signup").post(signup);
+router.route("/login").post(login);
 
 export default router;
