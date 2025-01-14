6ba31c26c44deb09a2b59ecbc41e17417ec12461
Muh-Uzair <muhammaduzair1062001@gmail.com>
2025-01-14 23:30:43 +0500

    faltu commit

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -4,12 +4,36 @@ import { globalAsyncCatch } from "../utils/global-async-catch";
 import jwt from "jsonwebtoken";
 import { AppError } from "../utils/app-error";
 import bcrypt from "bcryptjs";
+import { type } from "node:os";
 
-//FUNCTION
+interface interfaceDecodedToken {
+  id: string;
+  iat: number;
+  exp: number;
+}
+
+// FUNCTION
 const signToken = (id: string): string | null => {
-  return id && process.env.JWT_SECRET
-    ? jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" })
-    : null;
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+  const token: string = jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: 86400000,
+  });
+
+  const encodedToken = jwt.decode(token);
+  console.log(
+    `secret-type ${typeof process.env.JWT_SECRET} secret-length ${
+      process.env.JWT_SECRET.length
+    }`
+  );
+  console.log(process.env.JWT_SECRET);
+  console.log(`token-type${typeof token} token-length${token.length}`);
+  console.log(token);
+
+  console.log(encodedToken);
+
+  return token;
 };
 
 // FUNCTION
@@ -27,6 +51,7 @@ export const signup = async (
       throw new Error("JWT_SECRET is not defined");
     }
     const token: string | null = signToken(newUser.id);
+    console.log(token);
 
     if (!token) {
       return next(new AppError("Unable to generate jwt token", 500));
@@ -36,8 +61,7 @@ export const signup = async (
       status: "success",
       jwtToken: token,
       data: {
-        //www.youtube.com/watch?list=LL&v=9XaS93WMRQQ
-        https: newUser,
+        newUser,
       },
     });
   } catch (error: unknown) {
@@ -56,22 +80,15 @@ export const login = async (
     if (!req.body.email || !req.body.password) {
       next(new AppError("Please provide email and passwords", 400));
     } else {
-      console.log(req.body.email, req.body.password);
-
       // 2 : check is user exists
       const user: UserInterface | null = await UserModel.findOne({
         email: req.body.email,
       }).select("+password");
 
-      console.log(user);
-
       // 3 : compare the received password with password in db
       const passwordCorrect = user?.password
         ? await bcrypt.compare(req.body.password, user.password)
         : false;
-
-      console.log("_____________________________________________________");
-      console.log(passwordCorrect);
 
       // 4 : send response accordingly
       if (!user || !passwordCorrect) {
@@ -97,53 +114,41 @@ export const login = async (
 };
 
 // FUNCTION
-export const protect = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  try {
-    console.log(`${req.path} is protected`);
-
-    let token: string | null = null;
-    // 1 : get the token from the header received in the request
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      token = req.headers.authorization.split(" ")[1].trim();
-    }
-
-    if (!token) {
-      return next(new AppError("Provide token in request headers", 401));
-    }
-
-    // 2 : now if token exist than check that the token is not expired
-    // and also check that some has not changed the payload of token
-    console.log("step 2 : start");
-
-    // -> check the jwt secret exists
-    if (!process.env.JWT_SECRET) {
-      return next(new AppError("Invalid jwt secret", 401));
-    }
-
-    // --> verify the token
-    jwt.verify(token, process.env.JWT_SECRET.trim(), (error, decoded) => {
-      if (error) {
-        console.log(typeof process.env.JWT_SECRET);
-        console.log(token);
-        console.log(error);
-        return next(new AppError("Invalid jwt token", 401));
-      } else {
-        console.log(decoded);
-        console.log("step 2 : end");
-
-        next();
-      }
-    });
-  } catch (error: unknown) {
-    globalAsyncCatch(error, next);
+export const protect = (req: Request, res: Response, next: NextFunction) => {
+  let token: string | null = null;
+  // 1 : get the token from the header received in the request
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1].trim();
   }
+
+  if (!token) {
+    return next(new AppError("Provide token in request headers", 401));
+  }
+
+  // 2 : now if token exist than check that the token is not expired
+  // and also check that some has not changed the payload of token
+  console.log("step 2 : start");
+
+  // -> check the jwt secret exists
+  if (!process.env.JWT_SECRET) {
+    return next(new AppError("Invalid jwt secret", 401));
+  }
+  // --> verify the token
+  console.log(
+    `secret-type ${typeof process.env.JWT_SECRET} secret-length ${
+      process.env.JWT_SECRET.length
+    }`
+  );
+  console.log(process.env.JWT_SECRET);
+  console.log(`token-type ${typeof token} token-length ${token.length}`);
+  console.log(token);
+  const encodedToken1 = jwt.decode(token);
+  console.log(encodedToken1);
+
+  next();
 };
 
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3ODUxOWJjZWUwM2UwM2NlYjQwNTDNEpUTHQoQUJMHLrErGJyHg89uy71MyuHwIjoxNzM2ODYyNTI1fQ.5n7Z-rYzCQnpWUe3ppAc9RTNixht33gYNEUHcnNn6AI
